b463e92f90671ff90b00270121b1fe9221be1411
Антон <scouturl@mail.ru>
2017-05-30 05:55:39 +0300

    vse sdelal krome php mail...

---
+++ dom/mail-feed.php
@@ -0,0 +1,28 @@
+<?php     
+
+	$mail_admin = 'scoutrul@mail.ru';
+
+	$feed_email = $_POST['feed_email'];                 
+	$feed_name = $_POST['feed_name'];          
+	$feed_message = $_POST['feed_message'];   
+    
+
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= "Content-type: text/plain; charset=\"utf-8\"" . "\r\n";
+
+
+ 	$subject = 'Cообщение с сайта dom.naurasha.ru / от:  ' . $feed_name; 
+ 	
+	$body_message = 'DOM.NAURASHA.RU'. "\r\n";   
+ 	$body_message .= 'Имя: ' . $feed_name . "\r\n";      
+ 	$body_message .= 'Сообщение: ' . $feed_message;  
+ 	$headers .= 'From: '.$feed_name.' <'.$feed_email.'>' . "\r\n" .
+ 	 'Reply-To: <'.$feed_email.'>' . "\r\n" .
+ 	 'X-Mailer: PHP/' . phpversion();   
+
+	$mail_sent = mail($mail_admin, $subject, $body_message, $headers);               
+
+	header("Location: /#sent");
+
+?>
+
